c5dadf5838f20df22b0f7a8d537d7d027292aafb
csp <csp@qq.com>
2023-05-03 11:26:59 +0800

    add 添加 功能

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,14 +16,15 @@
             @selection-change="handleSelectionChange"
         >
             <el-table-column type="selection" width="55" />
-            <el-table-column fixed prop="id" label="ID" width="120" />
             <el-table-column prop="name" label="姓名" width="120" />
             <el-table-column prop="sex" label="性别" width="120" />
             <el-table-column prop="age" label="年龄" width="120" />
             <el-table-column prop="phone" label="手机号码" width="250" />
             <el-table-column fixed="right" label="操作" width="120">
                 <template #default>
-                    <el-button link type="primary" size="small" @click="handleRowClick">
+                    <el-button link type="primary" size="small" @click="handleRowClick"
+                    style="color: #ff1a44"
+                    >
                         删除
                     </el-button>
                     <el-button link type="primary" size="small">
@@ -33,12 +34,9 @@
             </el-table-column>
         </el-table>
        <!-- dialog       -->
-        <el-dialog v-model="dialogFormVisible" title="Shipping address" >
+        <el-dialog v-model="dialogFormVisible" :title="dialogType ==='add' ? '新增':'编辑'" >
 <!--            :label-position="labelPosition" :label-width="300"-->
             <el-form :model="tableForm" >
-                <el-form-item label="id" :label-width="100">
-                    <el-input v-model="tableForm.id" autocomplete="off" />
-                 </el-form-item>
                 <el-form-item label="姓名" :label-width="100">
                     <el-input v-model="tableForm.name" autocomplete="off" />
                 </el-form-item>
@@ -54,7 +52,7 @@
             </el-form>
             <template #footer>
             <span class="dialog-footer">
-                <el-button type="primary" @click="dialogFormVisible = false">
+                <el-button type="primary" @click="dialogConfirm">
                     确认
                 </el-button>
             </span>
@@ -65,8 +63,8 @@
 <script setup>
      import {ref} from "vue";
      /*数据*/
-     let queryInput=ref("")
-     let tableData=ref([
+     let queryInput=$ref("")
+     let tableData=$ref([
          {
          id: '1',
          name: '陈善平',
@@ -96,25 +94,36 @@
              phone:"18824682674"
          },
      ])
-     let multipleSelection=ref([])
-     let dialogFormVisible=ref(false)
-     let tableForm=ref({
+     let multipleSelection=$ref([])
+     let dialogFormVisible=$ref(false)
+     let tableForm=$ref({
          id:1,
          name:'张三',
          sex:'男',
          age:12,
          phone:"18824682674"
      })
+     let dialogType=$ref('add')
      /*方法*/
     let handleRowClick=()=>{
         console.log('click')
     }
      let handleSelectionChange = (val) => {
-         multipleSelection.value = val
+         multipleSelection = val
          console.log(val)
      }
      let handleAdd=()=>{
-         dialogFormVisible.value=true
+         dialogFormVisible=true
+         tableForm={}
+     }
+     let dialogConfirm=()=>{
+         dialogFormVisible=false
+
+         tableData.push({
+             id: (tableData.length+1).toString(),
+             ...tableForm
+         })
+         console.log(tableData)
      }
 
 </script>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue({
+        refTransform: true,
+        reactivityTransform: true,
+      })
+  ],
+  server :{
+    port: 8082
+  },
+  base: './'
 })
